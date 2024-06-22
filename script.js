433be41c4d57fd5dac8534476ab0736bfbae224d
swasti2611 <85633811+swasti2611@users.noreply.github.com>
2024-06-22 16:37:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // write js code here if required
+let p=document.getElementById("timer")
+let date=new Date();
+   const day=date.getDay();
+   const month=date.getMonth();
+   const year=date.getYear()
+
+const hour=date.getHours()
+const min=date.getMinutes();
+const sec=date.getSeconds()
+
+
+p.innerHTML=`
+  <div>${day}/${month}/${year},${hour}:${min}:${sec}</div>
+
+`
